5c861cbd6b18e5c93e57df2605c6365e6f027dfe
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-03 20:16:14 -0300

    chore: generic domain validator class

---
+++ src/Core/Domain/Validators/GenericDomainValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validators
+{
+    internal class GenericDomainValidator
+    {
+        private GenericDomainValidator() { }
+
+        public static void When(bool condition, Exception e)
+        {
+            if (!condition)
+                throw e;
+        }
+    }
+}
