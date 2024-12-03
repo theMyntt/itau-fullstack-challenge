df0e40851863779e744fe3ba1e2bc389d61cb4f6
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-03 20:27:06 -0300

    feat: client domain  entity

---
+++ src/Core/Domain/Entities/ClientEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Exceptions;
+using Domain.Validators;
+
+namespace Domain.Entities
+{
+    public class ClientEntity
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Participation { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        private ClientEntity(string firstName, string lastName, int participation, Guid? id = null, DateTime? createdAt = null) 
+        {
+            Id = id ?? Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            CreatedAt = createdAt ?? DateTime.UtcNow;
+        }
+
+        public static ClientEntity Build(string firstName, string lastName, int participation, Guid? id = null, DateTime? createdAt = null)
+        {
+            var entity = new ClientEntity(firstName, lastName, participation, id, createdAt);
+
+            ValidateDomain(entity);
+
+            return entity;
+        }
+
+        private static void ValidateDomain(ClientEntity entity)
+        {
+            GenericDomainValidator.When(entity.Participation < 1 || entity.Participation > 100, new ParticipationNotValid());
+            GenericDomainValidator.When(entity.CreatedAt > DateTime.UtcNow, new DateNotValid());
+        }
+    }
+}
