7e3c0fa137608831e5bbdee4b39b80abd1b715df
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-03 20:16:35 -0300

    chore: date and participation not valid exception

---
+++ src/Core/Domain/Exceptions/ParticipationNotValid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class ParticipationNotValid() : Exception("Participation needs to be >= 1 and <= 100");
+}
