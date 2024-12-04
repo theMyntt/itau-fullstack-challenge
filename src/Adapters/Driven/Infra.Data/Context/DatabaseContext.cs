e9dbc4b3b6848cdbd9894dfb9036378e0f6abd09
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-04 17:53:07 -0300

    chore: sql server context

---
+++ src/Adapters/Driven/Infra.Data/Context/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infra.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ClientModel> Clients { get; set; }
+    }
+}
