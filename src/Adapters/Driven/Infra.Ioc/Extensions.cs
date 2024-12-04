045e1057ae12f591839ff2e4d91300c5cb831fd1
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-04 18:10:59 -0300

    chore: creating db context

--- src/Adapters/Driven/Infra.Ioc/Extensions.cs
+++ src/Adapters/Driven/Infra.Ioc/Extensions.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infra.Ioc
 {
     public static class Extensions
     {
-        public static IServiceCollection AddExtensions(this IServiceCollection services)
+        public static IServiceCollection AddExtensions(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDatabaseContext(configuration);
+
+            return services;
+        }
+
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MSSQL") ?? throw new Exception("MSSQL IS NULL");
+
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(
+                connectionString,
+                m => m.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName)
+                ));
+
             return services;
         }
     }
