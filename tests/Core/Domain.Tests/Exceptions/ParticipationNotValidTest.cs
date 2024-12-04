5263952f665da9a7e4d60d9054d0496ed9484b64
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-03 21:20:05 -0300

    test: participation is not valid xunit

---
+++ tests/Core/Domain.Tests/Exceptions/ParticipationNotValidTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Exceptions;
+
+namespace Domain.Tests.Exceptions
+{
+    public class ParticipationNotValidTest
+    {
+        [Fact]
+        public async Task ShouldThrow()
+        {
+            var exception = await Assert.ThrowsAsync<ParticipationNotValid>(() => throw new ParticipationNotValid());
+            Assert.IsType<ParticipationNotValid?>(exception);
+        }
+
+        [Fact]
+        public async Task ShouldHaveCorrectMessage()
+        {
+            var exception = await Assert.ThrowsAsync<ParticipationNotValid>(() => throw new ParticipationNotValid());
+            Assert.Equal("Participation needs to be >= 1 and <= 100", exception.Message);
+        }
+    }
+}
