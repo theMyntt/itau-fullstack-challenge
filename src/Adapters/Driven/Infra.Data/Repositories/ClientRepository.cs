5d645bf73b23d7c43b804d041346c3094050e5e7
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 14:21:23 -0300

    feat: client repository

---
+++ src/Adapters/Driven/Infra.Data/Repositories/ClientRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions;
+using Domain.Entities;
+using Infra.Data.Abstractions;
+using Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly DatabaseContext _context;
+        private readonly IClientMapper _mapper;
+
+        public ClientRepository(DatabaseContext context, IClientMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ClientEntity> AddClientAsync(ClientEntity entity)
+        {
+            var model = _mapper.ToPersistance(entity);
+
+            await _context.Clients.AddAsync(model);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<ClientEntity>> GetClientsAsync(int page)
+        {
+            return await _context.Clients
+                .Skip((page - 1) * 20)
+                .Take(20)
+                .Select(model => _mapper.ToDomain(model))
+                .ToListAsync();
+        }
+    }
+}
---
+++ src/Core/Application/Abstractions/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Abstractions
+{
+    public interface IClientRepository
+    {
+        Task<ClientEntity> AddClientAsync(ClientEntity entity);
+        Task<IEnumerable<ClientEntity>> GetClientsAsync(int page);
+    }
+}
