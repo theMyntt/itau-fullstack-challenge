fe23ce86f18109aa6778a72d30bfae245f1f0ace
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 13:31:29 -0300

    chore: using scalar for api reference

--- src/Adapters/Driving/Presentations/Program.cs
+++ src/Adapters/Driving/Presentations/Program.cs
@@ -1,5 +1,6 @@
 using Presentations.Components;
 using Infra.Ioc;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +9,26 @@ builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.Environment
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddExtensions(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "/openapi/{documentName}.json";
+    });
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Cubo Itaú Full-Stack Challenge")
+            .WithSidebar(true);
+    });
+} // Configure the HTTP request pipeline.
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
