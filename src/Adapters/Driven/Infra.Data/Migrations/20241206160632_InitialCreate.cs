b7f5f550f3e80f6967fc927141de61579087e419
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 13:06:55 -0300

    chore: initial creation migration

---
+++ src/Adapters/Driven/Infra.Data/Migrations/20241206160632_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBL_CLIENTS",
+                columns: table => new
+                {
+                    ID_CLIENT = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TX_FIRST_NAME = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TX_LAST_NAME = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TX_PARTICIPATION = table.Column<int>(type: "int", nullable: false),
+                    TX_CREATED_AT = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBL_CLIENTS", x => x.ID_CLIENT);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBL_CLIENTS");
+        }
+    }
+}
