ab08ecbda65a8477467973de38144c8fb4d90c3e
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 14:13:15 -0300

    feat: client mapper to domain and to persistance

---
+++ src/Adapters/Driven/Infra.Data/Abstractions/IClientMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Infra.Data.Entities;
+
+namespace Infra.Data.Abstractions
+{
+    public interface IClientMapper
+    {
+        ClientEntity ToDomain(ClientModel model);
+        ClientModel ToPersistance(ClientEntity entity);
+    }
+}
---
+++ src/Adapters/Driven/Infra.Data/Repositories/ClientMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Infra.Data.Abstractions;
+using Infra.Data.Entities;
+
+namespace Infra.Data.Repositories
+{
+    public class ClientMapper : IClientMapper
+    {
+        public ClientEntity ToDomain(ClientModel model)
+        {
+            return ClientEntity.Build(
+                id: model.Id,
+                firstName: model.FirstName,
+                lastName: model.LastName,
+                participation: model.Participation,
+                createdAt: model.CreatedAt);
+        }
+
+        public ClientModel ToPersistance(ClientEntity entity)
+        {
+            return new ClientModel
+            {
+                Id = entity.Id,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                Participation = entity.Participation,
+                CreatedAt = entity.CreatedAt
+            };
+        }
+    }
+}
