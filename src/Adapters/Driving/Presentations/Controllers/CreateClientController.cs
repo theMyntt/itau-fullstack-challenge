97de463d1d3d47ff5275f7edaef5c9d36c333716
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-10 19:43:44 -0300

    chore: create client controller route

---
+++ src/Adapters/Driving/Presentations/Controllers/CreateClientController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions;
+using Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentations.Controllers
+{
+    [ApiController]
+    [Route("/api/client")]
+    [Tags("Client Management")]
+    public class CreateClientController : ControllerBase
+    {
+        private readonly ICreateClientUseCase _useCase;
+
+        public CreateClientController(ICreateClientUseCase useCase) => _useCase = useCase;
+
+        [HttpPost]
+        public async Task<IActionResult> Perform([FromBody] CreateClientDTO.Input input)
+        {
+            var result = await _useCase.CreateAsync(input);
+
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
