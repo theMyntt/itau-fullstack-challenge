d4b69c7fc40ec6defceb68d7f05d67a359766d40
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-12 09:07:08 -0300

    feat: table component

---
+++ src/Adapters/Driving/Presentations/Models/ClientModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Presentations.Models
+{
+    public class ClientModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Participation { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
