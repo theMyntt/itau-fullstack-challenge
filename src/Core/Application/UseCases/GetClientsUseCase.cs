4a51b66a5ed54d413a8edb29936037272595efab
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-13 21:16:46 -0300

    fix: db context can only have 1 thread

--- src/Core/Application/UseCases/GetClientsUseCase.cs
+++ src/Core/Application/UseCases/GetClientsUseCase.cs
@@ -19,13 +19,8 @@ namespace Application.UseCases
 
         public async Task<GetClientsDTO.Output> Run(GetClientsDTO.Input input)
         {
-            var resultsTask = _repository.GetClientsAsync(input.Page);
-            var tableSizeTask = _repository.CountAsync();
-
-            await Task.WhenAll(resultsTask, tableSizeTask);
-
-            var results = resultsTask.Result;
-            var sizeOfTable = tableSizeTask.Result;
+            var results = await _repository.GetClientsAsync(input.Page);
+            var sizeOfTable = await _repository.CountAsync();
              
             var totalPages = (int)Math.Ceiling((double)sizeOfTable / 20);
 
