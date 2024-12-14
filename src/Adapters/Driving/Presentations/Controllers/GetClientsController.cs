472f13ce498017586efd89e1cbbcddec49b24fa7
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-13 21:17:02 -0300

    feat: get clients controller

---
+++ src/Adapters/Driving/Presentations/Controllers/GetClientsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions;
+using Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentations.Controllers
+{
+    [ApiController]
+    [Route("/api/client")]
+    [Tags("Client Management")]
+    public class GetClientsController : ControllerBase
+    {
+        private readonly IGetClientsUseCase _useCase;
+
+        public GetClientsController(IGetClientsUseCase useCase) => _useCase = useCase;
+
+        [HttpGet]
+        public async Task<IActionResult> Perform([FromQuery] GetClientsDTO.Input input)
+        {
+            var result = await _useCase.Run(input);
+
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
